3ffc954f1eb3c4fe320928886a16d80da06b8e52
NekPatel <24bit188@sot.pdpu.ac.in>
2025-01-26 19:57:40 +0530

    Update and rename Tszz.py to rectangle.py

--- Tszz.py
+++
@@ -1,6 +0,0 @@
-length = 4
-breath = 6
-area = length * breath
-perimeter = 2 * (length + breath)
-print(area)
-print(perimeter)
---
+++ rectangle.py
@@ -0,0 +1,6 @@
+length = 4
+breath = 6
+area = length * breath
+perimeter = 2 * (length + breath)
+print("area of rectangle is ",area)
+print("perimeter of rectangle is ", perimeter)
