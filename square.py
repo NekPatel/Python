66565492c2711699ebd939bb802f30855e1475b2
NekPatel <24bit188@sot.pdpu.ac.in>
2025-01-26 20:06:59 +0530

    Update and rename zFrJ.py to square.py

---
+++ square.py
@@ -0,0 +1,5 @@
+length = 5
+area = length * length
+perimeter = 4 * length
+print("area of square is ",area)
+print("perimeter of sqaure is", perimeter)
--- zFrJ.py
+++
@@ -1,5 +0,0 @@
-length = 5
-area = length * length
-perimeter = 4 * length
-print(area)
-print(perimeter)
